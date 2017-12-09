a89fa8a8efc7c9e8a96a8e89dce0ec958f993f23
Zach Deibert <zachdeibert@gmail.com>
2017-12-08 21:40:57 -0600

    Upgraded the AI algorithm

--- Ai/NearestNeighbor/PreviousMoves/NearestNeighborPreviousMovesAi.cs
+++ Ai/NearestNeighbor/PreviousMoves/NearestNeighborPreviousMovesAi.cs
@@ -1,23 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Com.GitHub.ZachDeibert.RockPaperScissors.Ai.NearestNeighbor.PreviousMoves {
     public class NearestNeighborPreviousMovesAi : NearestNeighborAiBase {
-        Vector current;
+        const int PreviousMovesToConsider = 10;
+        IEnumerable<double> Knowledge;
 
-        protected override int Number => 1;
+        protected override int Number => 10;
 
-        protected override Vector Current => current;
+        protected override Vector Current => Knowledge.Count() >= 3 * PreviousMovesToConsider ? new Vector {
+            Components = Knowledge.ToArray()
+        } : null;
 
         protected override double Initial => 0;
 
         protected override double ResultToValue(Game game) {
-            current = new Vector {
-                Components = new double[] {
-                    game.Player == GameOption.Rock ? 1 : 0,
-                    game.Player == GameOption.Paper ? 1 : 0,
-                    game.Player == GameOption.Scissors ? 1 : 0
-                }
-            };
+            Knowledge = new double[] {
+                game.Player == GameOption.Rock ? 1 : 0,
+                game.Player == GameOption.Paper ? 1 : 0,
+                game.Player == GameOption.Scissors ? 1 : 0
+            }.Concat(Knowledge).Take(3 * PreviousMovesToConsider);
             return (((int) game.Player) + 1) % 3;
         }
 
@@ -29,6 +32,10 @@ namespace Com.GitHub.ZachDeibert.RockPaperScissors.Ai.NearestNeighbor.PreviousMo
                 value -= 3;
             }
             return (GameOption) Math.Round(value);
+        }
+
+        public NearestNeighborPreviousMovesAi() {
+            Knowledge = new double[0];
         }
     }
 }
